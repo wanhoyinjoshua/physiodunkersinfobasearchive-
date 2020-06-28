4f62f20a1b963f6eafc1c3870995a58858d8481f
joshua <wanhoyinsjoahu@yahoo.com>
2020-06-28 18:32:04 +1000

    hi

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ app.use(function(req, res, next) {
   if(process.env.NODE_ENV==="production"){
   
     app.use(express.static('my-app/build'))
-    app.get('*',(req,res)=>{res.sendFile(path.join(__dirname,'my-app','build','index.html'))})
+    app.get('/',(req,res)=>{res.sendFile(path.join(__dirname,'my-app','build','index.html'))})
   
 
   
